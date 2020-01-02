d94cbd30f262d3b145c45ddd8e4c9d2cf54887ac
salvatorelomonaco <lommetto95@gmail.com>
2020-01-02 15:27:56 +0100

    Avanzamento

--- main.js
+++ main.js
@@ -4,29 +4,61 @@ $(document).ready(function() {
     var template = $('#template').html();
     var templateFunction = Handlebars.compile(template);
 
-    $('button').click(function () {
-        $('.book-container').empty();
-        var ricerca = $('input').val();
-        bookApiCall(ricerca);
-    })
+    var templateBottom = $('#templateBottom').html();
+    var templateBottomFunction = Handlebars.compile(templateBottom);
+
+    $('#search_icon').click(function () {
+        $('#searchbar').animate({width:'toggle'},350);
+        // $('.top.section').empty();
+        // $('.bottom.section').empty();
+        // var ricerca = $('input').val();
+        // bookApiCall(ricerca);
+    });
 
     $('input').keypress(function (event) {
         if (event.which == 13) {
-            $('.book-container').empty();
+            $('.intro').hide();
+            $('.arrow i').show();
+            $('.top.section').empty();
+            $('.bottom.section').empty();
             var ricerca = $('input').val();
             bookApiCall(ricerca);
         }
-    })
+    });
+
+    //carosello:
+    //aggiungiamo classe show all'u;ltimo libro
+    //intercettiamo click su freccia
+    $('.arrow.right i').click(function(){
+        $('.top.section').append($('.book-card').eq(0));
+        var infoCorrente = $('.info-container.active');
+        var infoSucc = infoCorrente.next();
+        infoCorrente.removeClass('active');
+        infoSucc.addClass('active');
+        $('.bottom.section').append($('.info-container').eq(0));
+    });
+
+    $('.arrow.left i').click(function(){
+        // BISOGNA USCIRE DAI 39 FISSI!!!!
+        $('.top.section').prepend($('.book-card').eq(39));
+        $('.bottom.section').prepend($('.info-container').eq(39));
+        var infoCorrente = $('.info-container.active');
+        var infoPrev = infoCorrente.prev();
+        infoCorrente.removeClass('active');
+        infoPrev.addClass('active');
+    });
 
     function bookApiCall(ricercaUtente) {
         $.ajax({
             'url': 'https://www.googleapis.com/books/v1/volumes?q=' + ricercaUtente,
             'method': 'GET',
             'data':{
-                'key':'AIzaSyAGHLZ08VPW8NW1rwJELaYO1vnBLThiyKE'
+                'key':'AIzaSyAGHLZ08VPW8NW1rwJELaYO1vnBLThiyKE',
+                'maxResults': 40
             },
             'success': function(data) {
                 // cicliamo il data .items che ci restituisce un libro ad ogni ciclio
+                console.log(data);
                 var books = data.items;
                 bookGenerator(books);
             },
@@ -60,8 +92,13 @@ $(document).ready(function() {
                     }
                     var fullStar = '<i class="fas fa-star"></i>';
                     var emptyStar = '<i class="far fa-star"></i>';
-                    var properties = {
-                        'img':  currentBook.imageLinks.thumbnail,
+                    var container = $('.bottom.section');
+                    var classActive = '';
+                    // qui mettiamo il codice per regolare le classi active
+                    if ((container).is(':empty')) {
+                        var classActive = 'active';
+                    }
+                    var propertiesBottom = {
                         'title': currentBook.title,
                         'author': str,
                         'publishedDate': currentBook.publishedDate.slice(0, 4),
@@ -70,10 +107,16 @@ $(document).ready(function() {
                         'description': currentBook.description,
                         'publisher': currentBook.publisher,
                         'pageCount':  currentBook.pageCount,
-                        'categories': currentBook.categories
+                        'categories': currentBook.categories,
+                        'class': classActive
                     }
-                    var html = templateFunction(properties);
-                    $('.book-container').append(html);
+                    var properties = {
+                        'img':  currentBook.imageLinks.thumbnail
+                    };
+                    var topSection = templateFunction(properties);
+                    var bottomSection = templateBottomFunction(propertiesBottom);
+                    $('.top.section').append(topSection);
+                    $('.bottom.section').append(bottomSection);
                 },
                 'error': function() {
                 }
@@ -84,7 +127,9 @@ $(document).ready(function() {
 
 $(document).on('click', '.book-card', function() {
     $('.info-container').removeClass('active');
-    $(this).next('.info-container').addClass('active');
+    $('.book-card').removeClass('opacity');
+    $(this).addClass('opacity');
+    var that = $(this);
+    var current = $('.top.section').find(that).index();
+    $('.bottom.section').find('.info-container').eq(current).addClass('active');
 })
-
-// la prima carda che appare abbia la classe active
